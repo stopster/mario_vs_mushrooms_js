3d241e2ed6fa090c9912524fbff383c678b1bc8d
stopster <stopster17@gmail.com>
2013-10-17 23:53:42 +0300

    Added ground level, running and jumping

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,28 @@
 (function (d, w){
+	var utils = {
+		typeOf: function(object){
+			var strings = {
+				"[object Array]": "array",
+				"[object Object]": "object",
+				"[object String]": "string"
+			};
+
+			return strings[Object.prototype.toString.call(object)];
+		}
+	};
+
 	function Game(){
 		var self = this;
 		var running = false;
 
 		this.canvas = new GameCanv();
-		this.player = new Player(this.canvas);
+		this.controls = new GameControls();
+		this.player = new Player(this.canvas, this.controls);
 
 		this.start = function(){
 			running = true;
 			var start = 0;
-			var delay = 30; // 30 miliseconds between
+			var delay = 10; // 30 miliseconds between
 			window.requestAnimationFrame(function gameLoop(time){
 				if((time - start) > delay){
 					start = time;
@@ -33,6 +46,9 @@
 		var layers = [];
 		var canvas = d.getElementById("canvas");
 		var ctx = canvas.getContext("2d");
+
+		this.height = canvas.height;
+		this.width = canvas.width;
 
 		this.getCtx = function(){
 			return ctx;
@@ -58,13 +74,17 @@
 
 				layer.update(canvas, ctx);
 			}
+
+			ctx.moveTo(0, 300);
+			ctx.lineTo(canvas.width, 300);
+			ctx.stroke();
 		};
 	}
 
 	function CanvObj(gameCanv){
 		console.log("init canvas object");
 
-		var x, y, index=0;
+		var x, y, width, height, index=0;
 		this.canvas = gameCanv;
 		this.ctx = gameCanv.getCtx();
 
@@ -81,33 +101,65 @@
 		};
 	}
 
-	function Player(gameCanv){
-		console.log("init player");
+	function Player(gameCanv, controls){
 		var self = this;
 
 		CanvObj.call(this, gameCanv);
+		this.controls = controls;
 		this.x = 0;
 		this.y = 0;
 		this.width = 10;
 		this.height = 10;
+		this.runDir = 0;
+		this.grounded = true;
+		this.speed = 5;
+		this.jumpImpulse = 1;
+		this.gravity = 0.1;
+		this.groundLevel = this.canvas.height - this.height - 140;
+		this.y = this.groundLevel - this.height;
 
-		d.addEventListener("keydown", function(e){
-			switch(e.keyCode){
-				case 37:
-					self.x -=5;
-					break;
-				case 38:
-					self.y -=5;
-					break;
-				case 39:
-					self.x +=5;
-					break;
-				case 40:
-					self.y +=5;
-					break;
+		this.controls.addListener(["left", "right"], function(key, action){
+			if(action === "keydown"){
+				self.run(key);
+			} else{
+				self.stop();
 			}
 		});
+
+		this.controls.addListener(["space", "up"], function(key, action){
+			if(action === "keydown"){
+				self.jump();
+			}
+		});
+
+		this.run = function(dir){
+			this.runDir = (dir === "left")? -1: +1;
+		};
+
+		this.stop = function(){
+			this.runDir = 0;
+		};
+
+		this.jump = function(){
+			this.grounded = false;
+		};
+
 		this.update = function(){
+			this.x += this.runDir*this.speed;
+			if(!this.grounded){
+				this.jumpImpulse -= this.gravity;
+				this.y -= this.jumpImpulse*this.speed;
+				if(this.y >= this.groundLevel - this.height){
+					this.grounded = true;
+					this.jumpImpulse = 2;
+				}
+				
+			}
+
+			this.draw();
+		};
+
+		this.draw = function(){
 			this.ctx.fillStyle = "black";
 			this.ctx.fillRect(this.x, this.y, this.width, this.height);
 		};
@@ -158,6 +210,40 @@
 			39: "right",
 			40: "down"
 		};
+
+		var self = this;
+
+		this.listeners = {};
+		for(var key in keys){
+			this.listeners[keys[key]] = [];
+		}
+
+		this.addListener = function(keys, callback){
+			console.log(keys);
+			var keysToAdd = utils.typeOf(keys) === "array"? keys: [keys];
+			for(var i=0, ii=keys.length; i<ii; i++){
+				if(this.listeners[keys[i]] !== undefined){
+					this.listeners[keys[i]].push(callback);
+				}
+			}
+		};
+		this.trigger = function(key, action){
+			if(this.listeners[key] === undefined){
+				return;
+			}
+			var listeners = this.listeners[key];
+			for(var i=0, ii = listeners.length; i<ii; i++){
+				listeners[i](key, action);
+			}
+		};
+
+		d.addEventListener("keydown", function(e){
+			self.trigger(keys[e.keyCode], "keydown");
+		});
+
+		d.addEventListener("keyup", function(e){
+			self.trigger(keys[e.keyCode], "keyup");
+		});
 		console.log("init controls");
 	}
 
